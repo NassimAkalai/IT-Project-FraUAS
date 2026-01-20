e4037f7d722aa2ebc5ecf76fcc837b570360f3a7
Nassim <nassimakalai@outlook.de>
2026-01-20 12:33:58 +0100

    Update virus_spread_dfs.py

--- python_source_code/virus_spread_dfs.py
+++ python_source_code/virus_spread_dfs.py
@@ -82,7 +82,7 @@ def generate_task_for_dfs_virus(question_number, num_calls = 30, matrix_size = 5
     return result
 
 # Call
-folder_path = "C:\\Users\\akalai\\Desktop\\SoftwareProject-HayberKresovic-main (1)\\virus_dfs_xml"
+folder_path = "YOUR_PATHWAY_HERE"
 clear_variable_declarations(folder_path)
 
 num_calls = 30
@@ -90,3 +90,4 @@ question_number = 1
 
 result = generate_task_for_dfs_virus(question_number, num_calls, matrix_size = 5)
 format_to_xml(folder_path, result, question_number, num_calls)
+
